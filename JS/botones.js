0aafc4eb9ffa7bdb5510670047c25d68e5dec6cd
Pilar Flores <122378212+Pii005@users.noreply.github.com>
2024-05-20 20:39:47 -0300

    Update botones.js

--- JS/botones.js
+++ JS/botones.js
@@ -1,16 +1,13 @@
-export function mostrarBotones() {
-    action(true);
-}
-
-// Función para ocultar el spinner
 export function ocultarBotones() {
-    action();
+    document.getElementById('btn-edit').style.display = 'none';
+    document.getElementById('btn-back').style.display = 'none';
+    document.getElementById('btn-save-all').style.display = 'flex';
+    document.getElementById('btn-delete-all').style.display = 'flex';
 }
 
-function action(visible = false) {
-    // Mostrar 'btn-edit' y ocultar 'btn-save-all'
-    document.getElementById('btn-edit').style.display = visible ? 'flex' : 'none';
-    document.getElementById('btn-back').style.display = visible ? 'flex' : 'none';
-    document.getElementById('btn-save-all').style.display = visible ? 'none' : 'flex';
-    document.getElementById('btn-delete-all').style.display = visible ? 'none' : 'flex';
+export function mostrarBotones() {
+    document.getElementById('btn-edit').style.display = 'flex';
+    document.getElementById('btn-back').style.display = 'flex';
+    document.getElementById('btn-save-all').style.display = 'none';
+    document.getElementById('btn-delete-all').style.display = 'none';
 }
